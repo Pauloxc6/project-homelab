999e03e01f3f63e85b0773922c5c692ccab374ab
Pauloxc6 <paulo8756sp1@gmail.com>
2025-05-18 19:45:14 -0300

    Update ports tcp

--- src/docker/portainer/portainer.sh
+++ src/docker/portainer/portainer.sh
@@ -25,7 +25,7 @@ echo "[+] Criando o volume portainer_data"
 docker volume create portainer_data
 
 echo "[+] Run Container"
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+docker run -d -p 9000:8000 -p 8443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
 
 #================
 # Saida
